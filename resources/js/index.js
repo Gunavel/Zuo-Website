73178bc7763510b698fe57a96ce95bf2e15349fe
santheepMadhavan <adamsantheep18@gmail.com>
2021-05-15 02:06:42 +0530

    webpack added

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,10 @@
+import "../css/index.scss";
 const rightButton = document.getElementById("right");
 const leftButton = document.getElementById("left");
 const cardContainer = document.getElementsByClassName(
   "carousal-card-container"
 )[0];
-function activateCarousal() {
-  rightButton.click();
-}
+
 rightButton.addEventListener("click", (e) => {
   cardContainer.scrollTo({
     top: 0,
@@ -21,3 +20,5 @@ leftButton.addEventListener("click", (e) => {
     behavior: "smooth",
   });
 });
+
+rightButton.click();
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./resources/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
